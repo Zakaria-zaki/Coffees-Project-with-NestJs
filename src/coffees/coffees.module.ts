dc2535180fd6f36b1ad46340f3fa1ee75ccf28e1
zattaoui <03zaki95@gmail.com>
2021-12-21 00:14:01 +0100

    My first commit for the five chapter of my project

---
+++ src/coffees/coffees.module.ts
@@ -0,0 +1,20 @@
+import { Module, Scope } from '@nestjs/common';
+import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+import { Event } from '../events/entities/event.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Coffee, Flavor, Event]),
+    ConfigModule.forFeature(coffeesConfig),
+  ],
+  controllers: [CoffeesController],
+  providers: [CoffeesService, ConfigService],
+  exports: [CoffeesService],
+})
+export class CoffeesModule {}
